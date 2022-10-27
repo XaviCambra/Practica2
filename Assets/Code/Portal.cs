7f946d01b8c59d1a859a27edbe8f72a2fd09678a
XaviCambra <l4sixx@gmail.com>
2022-10-27 18:07:33 +0200

    Intento de que los portales funcionen

--- Assets/Code/Portal.cs
+++ Assets/Code/Portal.cs
@@ -8,6 +8,7 @@ public class Portal : MonoBehaviour
     public Transform m_OtherPortalTransform;
     public Portal m_MirrorPortal;
     public FPPlayerController m_Player;
+    public float m_OffsetNearPlane;
 
     private void LateUpdate()
     {
@@ -18,5 +19,8 @@ public class Portal : MonoBehaviour
         Vector3 l_WorldDirection = m_Player.m_Camera.transform.forward;
         Vector3 l_LocalDirection = m_OtherPortalTransform.InverseTransformDirection(l_WorldDirection);
         m_MirrorPortal.m_Camera.transform.forward = m_MirrorPortal.transform.TransformDirection(l_LocalDirection);
+
+        float l_Distance = Vector3.Distance(m_MirrorPortal.m_Camera.transform.position, m_MirrorPortal.transform.position);
+        m_MirrorPortal.m_Camera.nearClipPlane = l_Distance + m_OffsetNearPlane;
     }
 }
