d7be9b3866a6b1eff46bbb1cde3b8e4fb76320c1
Sora <bmartinezf@edu.tecnocampus.cat>
2022-11-14 17:19:34 +0100

    torretas

---
+++ Assets/Code/RefractionCube.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class RefractionCube : MonoBehaviour
+{
+    public LineRenderer m_Laser;
+    public LayerMask m_LaserLayerMask;
+    public float m_MaxLaserDistance = 250.0f;
+    bool m_RefractionEnabled = false;
+
+    private void Update()
+    {
+        m_Laser.gameObject.SetActive(m_RefractionEnabled);
+        m_RefractionEnabled=false;
+    }
+    public void CreateRefraction()
+    {
+        if (m_RefractionEnabled)
+            return;
+        m_RefractionEnabled=true;
+        Ray l_Ray = new Ray(m_Laser.transform.position, m_Laser.transform.forward);
+        float l_LaserDistance=m_MaxLaserDistance;
+        RaycastHit l_RayCastHit;
+        if(Physics.Raycast(l_Ray, out l_RayCastHit, m_MaxLaserDistance, m_LaserLayerMask.value))
+        {
+            l_LaserDistance = Vector3.Distance(m_Laser.transform.position, l_RayCastHit.point);
+            if (l_RayCastHit.collider.tag == "RefractionCube")
+                l_RayCastHit.collider.GetComponent<RefractionCube>().CreateRefraction();
+        }
+        m_Laser.SetPosition(1, new Vector3(0.0f, 0.0f, l_LaserDistance));
+    }
+}
---
+++ Assets/Code/Turret.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public LineRenderer m_Laser;
+    public LayerMask m_LaserLayerMask;
+    public float m_MaxLaserDistance = 250.0f;
+    public float m_AliveAngleInDegrees = 30.0f;
+
+    private void Update()
+    {
+        bool l_LaserAlive=Vector3.Dot(transform.up,Vector3.up)>Mathf.Cos(m_AliveAngleInDegrees*Mathf.Deg2Rad);
+        m_Laser.gameObject.SetActive(l_LaserAlive);
+        if (l_LaserAlive)
+        {
+            Ray l_Ray = new Ray(m_Laser.transform.position, m_Laser.transform.forward);
+                float l_LaserDistance = m_MaxLaserDistance;
+            RaycastHit l_RayCastHit;
+            if (Physics.Raycast(l_Ray, out l_RayCastHit, m_MaxLaserDistance, m_LaserLayerMask.value))
+            {
+                l_LaserDistance = Vector3.Distance(m_Laser.transform.position, l_RayCastHit.point);
+                if (l_RayCastHit.collider.tag == "RefractionCube")
+                    l_RayCastHit.collider.GetComponent<RefractionCube>().CreateRefraction();
+            }
+               
+            m_Laser.SetPosition(1, new Vector3(0.0f, 0.0f, l_LaserDistance));
+            //m_Laser.gameObject.SetActive(Vector3.Dot(transform.up, Vector3.up)>Mathf.Cos(m_AliveAngleInDegrees*Mathf.Deg2Rad));
+        }
+       
+    }
+
+    //hacer los set actives para las diferentes ocasiones
+}
